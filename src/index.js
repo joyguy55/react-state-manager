b40b5055a7f7564b64c6960edf41f3deb40b3d5f
Ryan <ryan.joyner@churchofjesuschrist.org>
2020-09-21 11:19:38 -0600

    Changed State Hook

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ export const ImpersonationsStateHook = (defaultImpersonations) => {
   );
 
   const syncImpersonations = (newImpersonation) => {
-    // TODO this will receive whatever the response is from an API Call
-    // This should have a full list of the current users impersonations I guess?
-    // That might be too much to send back after very call, unless we set limits?
-    console.log(newImpersonation);
     setImpersonation([...impersonations, newImpersonation]);
   };
 
